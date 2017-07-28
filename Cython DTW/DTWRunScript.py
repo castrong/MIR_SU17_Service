7bb29a356ae04d43c141ca37a2f69d6c4442a1d8
castrong <strongcreativesystems@gmail.com>
2017-07-28 09:44:22 -0700

    Delete DTWRunScript.py

--- Cython DTW/DTWRunScript.py
+++
@@ -1,5 +0,0 @@
-import pyximport; pyximport.install()
-import DTW_Cost_To_AccumCostAndSteps as DTW
-import numpy as np
-
-DTW.DTW_Cost_To_AccumCostAndSteps([[1,2,3],[4,5,6]], {'dn':[1,0,1], 'dm':[1,1,0], 'dw':[1,1,1] ,'SubSequence':True})
